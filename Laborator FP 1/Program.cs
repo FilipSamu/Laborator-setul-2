c2dadd47f2ac6ad2216df4e96a1d54ff493c71ce
FilipSamu <samu.filip2106@gmail.com>
2025-11-05 17:18:00 +0200

    Add files via upload

---
+++ Laborator FP 1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Introduceti numerele:");
+
+    string input=Console.ReadLine();
+string[] parts = input.Split(' ');
+int[] numereleintroduse = Array.ConvertAll(parts, int.Parse);
+
+foreach (int numar in numereleintroduse)
+{
+    if (numar % 2 == 0)
+    {
+        Console.WriteLine($"Numerele pare sunt: {numar}");
+
+    }
+
+}
+
+
+
